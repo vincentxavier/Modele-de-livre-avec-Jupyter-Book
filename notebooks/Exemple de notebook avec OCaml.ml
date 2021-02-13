3cdb96e50819d289f8b178b33359601e6652bb18
Lilian Besson <lilian.besson@crans.org>
2021-02-13 04:05:33 +0100

    update notebooks demo

--- notebooks/Exemple de notebook avec OCaml.ml
+++ notebooks/Exemple de notebook avec OCaml.ml
@@ -1,6 +1,37 @@
+(*
+This OCaml script was exported from a Jupyter notebook
+using an open-source software (under the MIT License) written by @Naereen
+from https://github.com/Naereen/Jupyter-Notebook-OCaml
+This software is still in development, please notify me of a bug at
+https://github.com/Naereen/Jupyter-Notebook-OCaml/issues/new if you find one
+*)
+
+(* # Exemple de notebook avec OCaml *)
+
+(* ## Explications
+
+Le kernel OCaml n'est pas installé par défaut avec Jupyter.
+
+Il faut installer OPAM, puis [ocaml-jupyter](https://github.com/akabe/ocaml-jupyter/). *)
+
+(* ## Exemples *)
+
+(* In[1]: *)
+
+
 Sys.command "ocaml -version";;
 
+(* In[2]: *)
+
+
 print_endline "Bonjour depuis OCaml !";;
+
+(* ### Une fonction récursive
+
+Pour calculer la fonction $n! := 1 \times 2 \times \dots \times n$ ($n\in\mathbb{N}$), on peut penser à une solution récursive (qui coutera en espace mémoire à cause de la pile d'appel) et une solution impérative. *)
+
+(* In[20]: *)
+
 
 let rec fact (n: int) : int =
     match n with
@@ -8,9 +39,17 @@ let rec fact (n: int) : int =
     | n -> n * (fact (n-1))
 ;;
 
+(* In[22]: *)
+
+
 for i = 1 to 21 do
     print_endline (Printf.sprintf "fact(%.2i) = %i" i (fact i))
 done;;
+
+
+(* Et la solution impérative : *)
+
+(* In[23]: *)
 
 
 let fact_imp (n: int) : int =
@@ -21,9 +60,19 @@ let fact_imp (n: int) : int =
     !f
 ;;
 
-for i = 1 to 21 do
+(* In[32]: *)
+
+
+for i = 15 to 21 do
     print_endline (Printf.sprintf "fact(%.2i) = %i" i (fact_imp i))
 done;;
+
+
+(* Comme les entiers sont bornés, on dépasse la capacité assez rapidement, et les valeurs calculées deviennent fausses. *)
+
+(* ### Un type non paramétrique récursif et un exemple : *)
+
+(* In[34]: *)
 
 
 type formulePropositionnelle =
@@ -33,10 +82,16 @@ type formulePropositionnelle =
     | Et of (formulePropositionnelle * formulePropositionnelle)
 ;;
 
+(* In[35]: *)
+
+
 let x = Var("x")
 and y = Var("y")
 and z = Var("z")
 ;;
+
+(* In[36]: *)
+
 
 let p1 = Ou(x, y)
 and p2 = Et(y, z)
@@ -47,21 +102,36 @@ let p4 = Et(p1, p2);;
 let p5 = Ou(p3, p4);;
 let p6 = Non(p5);;
 
+(* In[37]: *)
+
+
 let rec taille (formule: formulePropositionnelle) : int =
     match formule with
     | Var _ -> 1
     | Non phi -> 1 + taille phi
     | Et (phi1, phi2) | Ou (phi1, phi2) -> 1 + (taille phi1) + (taille phi2)
-    | _ -> 0
+    (* | _ -> 0 *) (* cette ligne est inutile *)
 ;;
+
+
+(* In[38]: *)
+
 
 taille x;;
 taille y;;
 taille z;;
 
+(* In[39]: *)
+
+
 taille p1;;
 taille p2;;
 taille p3;;
+
+(* Avec une fonction récursive terminale : *)
+
+(* In[40]: *)
+
 
 let taille (formule: formulePropositionnelle) : int =
     let rec aux acc = function
@@ -71,10 +141,31 @@ let taille (formule: formulePropositionnelle) : int =
     in aux 0 formule
 ;;
 
+(* In[41]: *)
+
+
 taille p4;;
 taille p5;;
 taille p6;;
 
+(* ### Un type paramétrique récursif et un exemple : *)
+
+(* In[42]: *)
+
+
 type 'a arbreBinaire = Feuille | Noeud of ('a arbreBinaire * 'a * 'a arbreBinaire);;
 
-Noeud(Feuille, 10, Feuille)
+(* In[43]: *)
+
+
+Noeud(Feuille, 10, Feuille);;
+
+(* ### D'autres exemples ?
+
+TODO: plus tard ! *)
+
+(* ## Pour en apprendre plus
+
+- Ce petit tutoriel : https://perso.crans.org/besson/apprendre-python.fr.html (sous licence GPLv3) ;
+- Ce WikiBooks : https://fr.wikibooks.org/wiki/Programmation_Python (sous licence libre) ;
+- Ces deux livres de Python au niveau lycée : https://github.com/exo7math/python1-exo7 et https://github.com/exo7math/python2-exo7 (sous licence Creative Commons). *)
--- notebooks/index.md
+++ notebooks/index.md
@@ -1,5 +1,12 @@
-Inclure des notebooks Jupyter dans différents langages
+# Inclure des notebooks Jupyter dans différents langages
 
 ```{warning}
 Work in progress...
 ```
+
+- Python
+- OCaml
+- C
+- Bash
+- Rust
+- Java
